8aad0ffae78f1b415d70494ee4e736a0fa1388af
Valeriy Van <github@w7software.com>
2018-09-26 01:31:06 +0200

    Initial Commit

---
+++ SignML/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SignML
+//
+//  Created by Valeriy Van on 9/26/18.
+//  Copyright © 2018 Valeriy Van. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
